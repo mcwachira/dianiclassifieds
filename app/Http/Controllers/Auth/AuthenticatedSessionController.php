79d71b281b416004b5fc62bf8e2852ffdb2b199d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2026-03-14 10:00:07 +0300

    Fixed issues with auth logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -36,7 +36,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('admin')->logout();
+        Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
