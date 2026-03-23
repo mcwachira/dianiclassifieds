471158933a04c3e7973a3a62c794e1c3972299eb
mcwachira <23430963+mcwachira@users.noreply.github.com>
2026-03-23 13:00:22 +0300

    Made some changes to the routes and layout

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('frontend.layouts.app');
+    return view('frontend.home.index');
 });
 
 Route::get('/dashboard', function () {
