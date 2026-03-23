ab4b9f251cc6bbe56fc3550c7262b9748f1227a9
mcwachira <23430963+mcwachira@users.noreply.github.com>
2026-03-23 11:50:46 +0300

    Made some changes to the app

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -61,7 +61,7 @@
     </div>
 </div>
 
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 <script src="{{asset('assets/admin/js/jsvectormap.min.js')}}" defer></script>
 
