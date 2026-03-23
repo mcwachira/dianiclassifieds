5815bd08e7de23cfbf992332c463b5d09368db8d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2026-03-23 18:33:41 +0300

    Created the User Dsshboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,14 @@ Route::get('/', function () {
     return view('frontend.home.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::group( ['middleware' => ['auth', 'verified']], function () {
+
+    Route::get('/dashboard', [UserDashboardController::class, 'index']) -> name('user.dashboard');
+});
+
 
 
 Route::get('/admin/dashboard', function () {
